d262b4dc65c81a6dcd6d0082235b6c0e45d72d53
Georgia Tardy <georgiatardyart@gmail.com>
2017-08-02 17:40:00 -0400

    made corrections to main js to improve functionality, fixed event listener to play audio through audio player

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let search = document.querySelector("#submit");
-let tracks = document.querySelector("#results")
-
+let tracks = document.querySelector("#results");
+let audio = document.getElementById("audioplayer");
 
 // // ======Clears search to begin a new search when "Search Again" button is clicked==========
 
-clear.addEventListener('click', function () {
+clear.addEventListener('click', function() {
   tracks.innerHTML = ' ';
 })
 
@@ -13,7 +13,7 @@ clear.addEventListener('click', function () {
 
 submit.addEventListener('click', function() {
 
-// ====Fetch request from iTunes API to display list of songs from a search===
+  // ====Fetch request from iTunes API to display list of songs from a search===
 
   fetch(`https://itunes.apple.com/search?term==${input.value}`).then(function(response) {
       if (response.status === 200) {
@@ -25,17 +25,28 @@ submit.addEventListener('click', function() {
       tracks.innerHTML = '';
 
 
-// =====Loops through API data to retrieve search input======
-
+      // =====Loops through API data to retrieve search input======
+      let artist = document.getElementsByClassName("Artist_Bio")
       let results = data.results;
       for (i = 0; i < results.length; i++) {
-        tracks.innerHTML +=
-          `<div class="Artist_Bio">
-<h3>${results[i].artistName}</h3>
+        let innerBox = document.createElement("section");
+        tracks.appendChild(innerBox);
+        innerBox.className = "Artist_Bio"
+        innerBox.value = `${results[i].previewUrl}`
+        innerBox.innerHTML =
+          `<h3>${results[i].artistName}</h3>
 <h4>${results[i].trackName}</h4>
  <img src="${results[i].artworkUrl100}"></img>
-<a href="${results[i].trackViewUrl}">Listen to Song</a>
-</div>`;
+ <div style="display: none" class = "preview"  >${data.results[i].previewUrl}</div>
+
+`;
+
+        console.log(artist.value);
+        innerBox.addEventListener("click", function() {
+          audioplayer.src = innerBox.value;
+        })
+
+
       }
     })
 });
